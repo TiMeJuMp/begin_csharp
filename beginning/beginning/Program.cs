21939a30c7b1e4b1cc42db9ab3cf73741d11722d
Becker, Christian <christian.becker@tecalliance.net>
2022-09-15 09:11:43 +0200

    created string variable and print it our #2

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -10,7 +10,8 @@ namespace beginning
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userName = "Christian";
+            Console.WriteLine("Hello " + userName);
             Console.ReadLine();
         }
     }
