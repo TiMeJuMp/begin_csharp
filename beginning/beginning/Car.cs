827dc96451cd9e4ff50562d27e4b26b2a3d09668
Becker, Christian <christian.becker@tecalliance.net>
2022-09-19 09:41:00 +0200

    start explaining Classes on example Car

---
+++ beginning/beginning/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace beginning
+{
+    public class Car
+    {
+        public int TypeId { get; set; }
+        public string Make { get; set;}
+        public string Model { get; set; }
+        public string TypeName { get; set; }
+        public int CCM { get; set; }
+        public string Color { get; set; }
+        public int Tyres { get; set; }
+
+        public Car(int typeId, string make, string model, string typeName, int cCM, string color, int tyres)
+        {
+            TypeId = typeId;
+            Make = make;
+            Model = model;
+            TypeName = typeName;
+            CCM = cCM;
+            Color = color;
+            Tyres = tyres;
+        }
+
+        public void PrintCarInformation()
+        {
+            Console.WriteLine($"Mein Fahrzeug ist ein " +
+                $"{Make} {Model} {TypeName} mit einem Hubraum von " +
+                $"{CCM} in der Farbe {Color} mit {Tyres} Reifen");
+        }
+    }
+}
--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,82 +13,25 @@ namespace beginning
     {
         static void Main(string[] args)
         {
-            Random r = new Random();
-            int budget = 10000;
-            bool wonLastRound = false;
-            int einsatz = 10;
-            bool zeroThrown = false;
-            int runden = 0;
-            int höchsteZahl = 0;
-            int kleinsteZahl = 40;
-            Console.WriteLine("Wir beginnen mit dem Geld verlieren!");
-            while (budget > 0)
+            Car c1 = new Car(13494, "Audi","A3","2.0 TDI", 1900, "Rot", 4);
+
+
+            Car c2 = new Car(56553, "Volkswagen", "Golv V", "1.9 TDI", 1890, "Grau Metallic",4);
+
+            List<Car> cars = new List<Car>() { c1, c2};
+
+            foreach(Car car in cars)
             {
-                Console.WriteLine($"Dein Aktuelles Geld {budget}");
-                if (zeroThrown == false)
-                {
-                    if (wonLastRound && budget > 10000)
-                    {
-                        einsatz = 10;
-                    }
-                    else if (!wonLastRound && budget > 10000)
-                    {
-                        einsatz = einsatz * 2;
-                    }
-                    else if (budget < 10000)
-                    {
-                        int verlust = 10000 - budget;
-                        einsatz = verlust * 2;
-                    }
-
-                    //Setze maximal 1000$
-                    einsatz = Math.Min(einsatz, 1000);
-                    if (einsatz > budget)
-                    {
-                        einsatz = budget;
-                    }
-                }
-                Console.WriteLine($"Wir setzen {einsatz}");
-
-                budget = budget - einsatz;
-                runden++;
-
-
-
-                int number = r.Next(0, 37);
-                if(number < kleinsteZahl)
-                {
-                    kleinsteZahl = number;
-                }
-                if(number > höchsteZahl)
-                {
-                    höchsteZahl = number;
-                }
-                Console.WriteLine($"Wir schmeißen {number}");
-                zeroThrown = false;
-                if (number % 2 == 0 && number != 0 ) //k gerade, gewonnen
-                {
-                    wonLastRound = true;
-                    budget = budget + einsatz * 2;
-                    einsatz = einsatz * 2;
-                    Console.WriteLine($"Wir haben gewonnen {einsatz * 2}");
-                }
-                else if(number == 0) // k ist 0
-                {
-                    zeroThrown = true;
-                    Console.WriteLine($"Null geschmissen, Einsatz bleibt gleich {einsatz}");
-                }
-                else
-                {
-                    Console.WriteLine($"Wir haben verloren");
-
-                }
-               // Thread.Sleep(1500);
+                car.PrintCarInformation();
             }
-            Console.WriteLine($"Du hast nach {runden} Runden dein Geld verzockt.");
-            Console.WriteLine($"Höchste Zahl {höchsteZahl}:    Kleinste Zahl: {kleinsteZahl}");
+
             Console.ReadLine();
+            
         }
+
+
+
+        
 
     }
 }
