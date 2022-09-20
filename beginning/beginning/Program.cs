351f7067ae56b75165cec73d7d51a3860ba74190
Becker, Christian <christian.becker@tecalliance.net>
2022-09-20 15:22:40 +0200

    csv reader

--- beginning/beginning/Program.cs
+++ beginning/beginning/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -13,38 +14,61 @@ namespace beginning
     {
         static void Main(string[] args)
         {
-            Dreieck d = new Dreieck(2,4,10);
+            //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMI-data.csv");
 
-            Circle c = new Circle(5);
-            
-            Rechteck r = new Rechteck(10,20);
 
-            List<IGeometry> geometries = new List<IGeometry>();
-            geometries.Add(d);
-            geometries.Add(c);
-            geometries.Add(r);
+            //fi.CopyTo("C:/050 Ausbilder/03 Ausbildung 2022/begin_csharp/beginning/beginning/meine_datei13.csv");
+            //FileInfo fi_neu = new FileInfo("C:/050 Ausbilder/03 Ausbildung 2022/begin_csharp/beginning/beginning/meine_datei13.csv");
+            //fi_neu.Delete();
 
-            foreach(IGeometry g in geometries)
+
+            //Console.WriteLine("Gebe mir den pfad an, wohin die Datei geschrieben werden soll!");
+            //string path = Console.ReadLine();
+            //path = path.Replace("\\", "\\\\");
+            //DirectoryInfo di = new DirectoryInfo(path.Replace("\\", "\\\\"));
+
+            //while(di.Exists == false)
+            //{
+            //    Console.WriteLine("Dein Pfad existiert nicht, gib mir einen neuen!");
+            //    path = Console.ReadLine();
+            //    di = new DirectoryInfo(path.Replace("\\", "\\\\"));
+            //}
+            //Console.WriteLine("Wie soll die Datei heißen?");
+            //string fileName = Console.ReadLine();
+
+            //fi.CopyTo(path +"\\"+ fileName);
+
+            //FileInfo fi = new FileInfo("T:/WKH_EXC/Trash/cbec/BMIS-data.csv");
+
+            //if(fi.Exists)
+            //{
+            //    //Execute only if file exists
+            //    fi.CopyTo("C:/050 Ausbilder/03 Ausbildung 2022/begin_csharp/beginning/beginning/meine_datei13111.csv");
+            //}
+
+            string path = @"C:\050 Ausbilder\03 Ausbildung 2022\begin_csharp\beginning\beginning\meine_datei123.csv";
+            StreamReader sr = new StreamReader(path);
+            List<string[]> eintraege = new List<string[]>();
+            //Remove Header
+            sr.ReadLine();
+            while(!sr.EndOfStream)
             {
-                Console.WriteLine($"Mein {g.GetType().Name} hat eine Fläche von {g.Flaeche()} " +
-                    $"und Umfang von {g.Umfang()}");
+                eintraege.Add(sr.ReadLine().Split(';'));
             }
-
-            List<Vehicle> fahrzeuge = new List<Vehicle>();
-            fahrzeuge.Add(new Motorcycle(523, "BMW", "GSXR 2000", "3.0", 3000, "Blau", 2));
-            fahrzeuge.Add(new Car(123, "VW", "Golf V", "2.0 TDI", 1900, "Rot", 4));
-
-            foreach(Vehicle v in fahrzeuge)
+            for (int j = 18; j < 101; j++)
             {
-                v.Starten();
-                v.Beschleunigen();
-                v.Beschleunigen();
-                v.Bremsen();
-                v.Starten();
-                v.Bremsen();
-                v.Bremsen();
+                using (StreamWriter writer = new StreamWriter($@"C:\050 Ausbilder\03 Ausbildung 2022\begin_csharp\Gewicht_{j}.csv", true))
+                {
+                    for (int i = 0; i < eintraege.Count; i++)
+                    {
+                        if (Convert.ToInt32(eintraege[i][2]) ==j)
+                        {
+                            writer.WriteLine(string.Join(";", eintraege[i]));
+                            Console.WriteLine(string.Join("\t", eintraege[i]));
+                        }
+                    }
+                }
             }
-            
 
             Console.ReadLine();            
         }
